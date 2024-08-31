dc38b4880ab94c38b6f09ca87eaee06fae2dd6a2
Ross0914 <thejakeman33@gmail.com>
2024-08-30 19:45:55 -0500

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,8 @@
+let count = 0;
+
+let addElement = document.getElementById("add");
+let subElement = document.getElementById("sub")
+let numberElement = document.getElementById("number")
+
+addElement.addEventListener ("click", addButton);
+subElement.addEventListener ("click", subButton);
